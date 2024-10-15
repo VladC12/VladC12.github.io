a5013246193869d6cded00d390638b00ad60e8ca
Vattsu <vladcrehul12@gmail.com>
2024-10-15 18:52:51 +0300

    Animate toggle

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -1,21 +1,32 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import styles from "./ThemeToggle.module.css";
 import { ThemeContext } from "../context/ThemeContext";
 
 const ThemeToggle: React.FC = () => {
 
   const { isDarkMode, setIsDarkMode } = useContext(ThemeContext)
+  const [iconAnimation, setIconAnimation] = useState("");
 
   const toggleTheme = () => {
-    setIsDarkMode(!isDarkMode);
+    setIconAnimation(styles.comeOut);
+    setTimeout(() => {
+      setIsDarkMode(!isDarkMode);
+      setIconAnimation(styles.comeIn);
+    }, 500); // Duration of the comeOut animation
+
+    setTimeout(() => {
+      setIconAnimation("");
+    }, 1000); // Duration of the comeOut + comeIn animations
   };
 
   return (
     <div className={styles.toggle} onClick={toggleTheme}>
       {isDarkMode ? (
-        <img src="/moon.svg" alt="Dark mode" className={styles.icon} />
+        <img src="/moon.svg" alt="Dark mode" 
+        className={`${styles.icon} ${iconAnimation}`} />
       ) : (
-        <img src="/sun.svg" alt="Light mode" className={styles.icon} />
+        <img src="/sun.svg" alt="Light mode" 
+        className={`${styles.icon} ${iconAnimation}`} />
       )}
     </div>
   );
