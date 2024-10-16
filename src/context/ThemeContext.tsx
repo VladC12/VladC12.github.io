e5c4a5339c2a1a8b007306516808a13e2f12c5c1
Vattsu <vladcrehul12@gmail.com>
2024-10-16 15:55:59 +0300

    Update transition duration in CurriculumVitae module and fix default dark mode in ThemeContext

--- src/context/ThemeContext.tsx
+++ src/context/ThemeContext.tsx
@@ -4,7 +4,9 @@ export const ThemeContext = createContext<any>({ theme: "light", undefined });
 
 export const ThemeProvider: React.FC<any> = ({ children }) => {
     const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)").matches;
-    const [isDarkMode, setIsDarkMode] = useState(prefersDarkScheme);
+    const defaultDarkMode = prefersDarkScheme || !window.matchMedia("(prefers-color-scheme: light)").matches;
+    
+    const [isDarkMode, setIsDarkMode] = useState(defaultDarkMode);
 
   return (
     <ThemeContext.Provider value={{ isDarkMode, setIsDarkMode }}>
