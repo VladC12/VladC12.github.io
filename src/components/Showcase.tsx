4403d0641d720fa31f9a6174704b93d803a2770f
Vattsu <vladcrehul12@gmail.com>
2024-10-16 21:33:34 +0300

    animate transition

--- src/components/Showcase.tsx
+++ src/components/Showcase.tsx
@@ -1,3 +1,6 @@
+import styles from "./Showcase.module.css";
+import Cradle from "./showcases/Cradle";
+
 import DefaultShowcase from "./showcases/DefaultShowcase";
 import { useEffect, useState } from "react";
 
@@ -7,10 +10,14 @@ interface Props {
 
 const Showcase: React.FC<Props> = ({ showcase }) => {
     const [displayValue, setDisplayValue] = useState<React.ReactNode>("default");
+    const [previousShowcase, setPreviousShowcase] = useState("");
+    const [animation, setAnimation] = useState("");
 
     const displayShowcase = () => {
         switch (showcase) {
             case 'cradle':
+                setPreviousShowcase(showcase);
+                return <Cradle />;
             case 'quartermaster':
             case 'caster':
             case 'event-manager':
@@ -20,6 +27,7 @@ const Showcase: React.FC<Props> = ({ showcase }) => {
             case 'thesis':
             case 'modbus':
             case 'edge-detection':
+                setPreviousShowcase(showcase);
                 return showcase;
             default:
                 return <DefaultShowcase />;
@@ -29,17 +37,39 @@ const Showcase: React.FC<Props> = ({ showcase }) => {
     useEffect(() => {
         if (showcase == "left") {
             const timer = setTimeout(() => {
-                setDisplayValue(<DefaultShowcase />);
+                setAnimation(styles.comeOut);
+                const innerTimer = setTimeout(() => {
+                    setDisplayValue(<DefaultShowcase />);
+                    setPreviousShowcase("left")
+                    setAnimation(styles.comeIn);
+                }, 500)
+                return () => {
+                    clearTimeout(innerTimer);
+                }
             }, 10000);
 
-            return () => clearTimeout(timer);
+            return () => {
+                clearTimeout(timer);
+            }
+
+        } else if (showcase != "landing" && showcase != previousShowcase) {
+            setAnimation(styles.comeOut);
+            const timer = setTimeout(() => {
+                setDisplayValue(displayShowcase());
+                setAnimation(styles.comeIn);
+            }, 400);
+
+            return () => {
+                clearTimeout(timer);
+            }
+
         } else {
             setDisplayValue(displayShowcase());
         }
     }, [showcase]);
 
     return (
-        <>{displayValue}</>
+        <div className={`${styles.container} ${animation}`}>{displayValue}</div>
     )
 }
 
---
+++ src/components/showcases/Cradle.tsx
@@ -0,0 +1,9 @@
+import styles from "./Showcases.module.css";
+
+const Cradle: React.FC = () => {
+  return (
+    <div className={styles.container}>Cradle</div>
+  )
+}
+
+export default Cradle
