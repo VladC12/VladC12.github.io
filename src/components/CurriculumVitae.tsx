1eb86d5a8f1a9c19fc9326b44832c88187de248a
Vattsu <vladcrehul12@gmail.com>
2024-11-06 02:04:10 +0200

    update CurriculumVitae styles and content; change hover effect background color, add bounce animation, and enhance project descriptions with bold formatting

--- src/components/CurriculumVitae.tsx
+++ src/components/CurriculumVitae.tsx
@@ -39,29 +39,34 @@ const CurriculumVitae: React.FC = () => {
                     <p>I am a <b>software engineer</b> with a passion for all things technology: software, hardware, electronics, and
                         artificial intelligence. My main professional focus is <b>web development</b>. Especially in <b>React</b> and <b>NextJS</b>.</p>
                 </div>
+                <h2>Skills</h2>
+                <p>TODO: Icons and whatnot for React, Python, Typescript, NextJS, NodeJS</p>
                 <h2>Experience</h2>
                 <ul>
                     <li>
                         <h3>09/2021 - 12/2024</h3>
                         <h3>SOFTWARE ENGINEER, SC Artificial Intelligence Visual Analytics SRL</h3>
                         <p>
-                            My first job, and it was a start-up. Worked on multiple projects as a full-stack developer, some for internal use only and some to be used by
-                            clients. Most of the work was focused on web development using React and NodeJS. Another
-                            part of the work there was in Synthetic Data Generation with Blender and Python and 3D
+                            My first job, and it was a start-up. Worked on multiple projects as a <b>full-stack developer</b>, some for internal use only and some to be used by
+                            clients. Most of the work was focused on web development using <b>React</b> and <b>NodeJS</b>. Another
+                            part of the work there was in Synthetic Data Generation with <b>Blender</b> and <b>Python</b> and 3D
                             modeling.
                         </p>
                         <h2>Projects</h2>
                         <ul className={styles.projects}>
                             <li id="cradle" onMouseEnter={handleHover} onMouseLeave={() => setShowcase("left")}>
                                 <h3>Cradle</h3>
-                                <p>Written in python, this application ingests RTSP streams from one or more sources, performing real-time inference on the video frames using a machine learning model. The inference results are then packaged into analytical payloads, which can include people detection data and other insights. This tool is designed for use in surveillance systems, traffic monitoring setups, and other scenarios where real-time video analysis can provide valuable insights.</p>
+                                <p>Written in <b>python</b>, this application ingests <b>RTSP streams</b> from one or more sources, 
+                                performing real-time inference on the video frames using a machine learning model. 
+                                The inference results are then packaged into analytical payloads, which can include people detection data and other insights. 
+                                This tool is designed for use in surveillance systems, traffic monitoring setups, and other scenarios where real-time video analysis can provide valuable insights.</p>
                                 <div>
-                                    <span>Python</span><span>OpenCV</span><span>Multiprocessing</span><span>Tensorflow</span>
+                                    <span>Python</span><span>OpenCV</span><span>Multiprocessing</span><span>Tensorflow</span><span>Kafka</span>
                                 </div>
                             </li>
                             <li id="quartermaster" onMouseEnter={handleHover} onMouseLeave={() => setShowcase("left")}>
                                 <h3>Quartermaster</h3>
-                                <p>One of the internal tools developed for AiVA was created using NextJS. Its purpose was to assist
+                                <p>One of the internal tools developed for AiVA was created using <b>NextJS</b>. Its purpose was to assist
                                     in organizing stored dataset materials by allowing mass uploads of files in a specific directory
                                     structure while handling upload chunking. It also catered to special cases such as organizing 3D
                                     model files that had texture files in folders. Videos, images, and 3D models could be viewed directly in the browser.
@@ -76,22 +81,22 @@ const CurriculumVitae: React.FC = () => {
                             </li>
                             <li id="caster" onMouseEnter={handleHover} onMouseLeave={() => setShowcase("left")}>
                                 <h3>Caster</h3>
-                                <p>Another internal tool developed for AiVA also created in NextJS and Flask. The web applications
+                                <p>Another internal tool developed for AiVA also created in <b>NextJS</b> and <b>Flask</b>. The web applications
                                     was meant to simplify the process of visually saving image coordinates. The main page of the web
-                                    application allowed users to save RTSP Video Streams with a thumbnail based on a screen-shot
+                                    application allowed users to save <b>RTSP Video Streams</b> with a thumbnail based on a screen-shot
                                     from the stream.
                                     For each stream one could create lines for direction and passage and polygons for regions of
                                     interest (ROI), using Canvas. These lines and polygons were used by AI to detect when people
                                     pass the lines or are present in the ROI. Some additional options were also available, although
                                     less important.
-                                    Flask was used to handle the RTSP streams and APIs related to OpenCV.</p>
+                                    <b>Flask</b> was used to handle the RTSP streams and APIs related to <b>OpenCV</b>.</p>
                                 <div>
                                     <span>NextJS</span><span>React</span><span>Typescript</span><span>Flask</span>
                                 </div>
                             </li>
                             <li id="event-manager" onMouseEnter={handleHover} onMouseLeave={() => setShowcase("left")}>
                                 <h3>Event Manager</h3>
-                                <p>One of the projects meant for client-use. It is created in NextJS. It is meant to visualize different
+                                <p>One of the projects meant for client-use. It is created in <b>NextJS</b>. It is meant to visualize different
                                     events that can happen at a checkout counter such as item scanning fraud.
                                     It shows a detailed table that can be filtered based on multiple factors.
                                     You can create users which have different limitations.</p>
@@ -101,8 +106,8 @@ const CurriculumVitae: React.FC = () => {
                             </li>
                             <li id="event-watcher" onMouseEnter={handleHover} onMouseLeave={() => setShowcase("left")}>
                                 <h3>Event Watcher</h3>
-                                <p>Another project for client-use. It was made with Electron and React. It’s a desktop app that allows
-                                    the user to be notified in real-time of item fraud at the checkout counter. Users can view a ~5
+                                <p>Another project for client-use. It was made with <b>Electron</b> and <b>React</b>. It's a desktop app that allows
+                                    the user to be notified in <b>real-time</b> of item fraud at the checkout counter. Users can view a ~5
                                     second video of the event, along with the SKU barcode, price, and timestamp. Associated with it
                                     there is Event Watcher Buddy which allows to manage the users. The user system is similar to
                                     Event Manager.
@@ -119,7 +124,7 @@ const CurriculumVitae: React.FC = () => {
                         <h3>INTERN SOFTWARE DEVELOPER, SC Continental Automotive Systems SRL</h3>
                         <p>
                             Participated at an internship organized by Continental where I developed software and did basic
-                            electronics in a team of four people, for prototype devices.
+                            <b>electronics</b> in a team of four people, for prototype devices.
                         </p>
                         <h2>Projects</h2>
                         <p>Most of the work was on the electronics side honestly.</p>
